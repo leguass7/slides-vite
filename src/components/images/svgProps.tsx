57d83cbe17c83cb49caa0bd09254cb63489d5bee
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2022-04-04 22:00:10 -0300

    feat: add logo svg

---
+++ src/components/images/svgProps.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from 'react'
+
+export const svgProps: SVGProps<SVGSVGElement> = {
+  xmlns: 'http://www.w3.org/2000/svg',
+  xmlSpace: 'preserve',
+  version: '1.1',
+  shapeRendering: 'geometricPrecision',
+  textRendering: 'geometricPrecision',
+  imageRendering: 'optimizeQuality',
+  fillRule: 'evenodd',
+  clipRule: 'evenodd',
+  xmlnsXlink: 'http://www.w3.org/1999/xlink'
+}
